60f72478ef3b1c897b2b949f49cd345951152d0b
Juan Saieh <juan.saieh@koombea.com>
2019-07-31 14:22:36 -0500

    Created delete view for Products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,16 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:destroy]
 
   def index
     @products = Product.all
   end
 
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
