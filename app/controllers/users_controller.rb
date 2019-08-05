b3d850286a204d6637d53b6a8784c43a63f5ff8b
Juan Saieh <juan.saieh@koombea.com>
2019-08-05 14:53:51 -0500

    Added styles to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    byebug
     if @user.save
       redirect_to root_path, notice: 'your User is now live'
     else
