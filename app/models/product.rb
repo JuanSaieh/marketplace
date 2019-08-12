2687dc95ab251b76fad6866c3f577b2b3c6e5978
Juan Saieh <juan.saieh@koombea.com>
2019-08-12 09:27:57 -0500

    added statuses for Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
   validates_presence_of :name, :description, :quantity, :price
+
+  enum status: { draft: 0, published: 1, archived: 2 }
 end
