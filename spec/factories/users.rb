43750653449dc3853948fae0726d38911098e664
Juan Saieh <juan.saieh@koombea.com>
2019-09-11 13:43:34 -0500

    added tests for user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    first_name { 'John' }
+    last_name { 'Doe' }
+    sequence(:email) { |n| "Email#{n}@example.com" }
+    password { 'gogogogo' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { is_expected.to be_a_new(User) }
+
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+  it { should_not validate_presence_of(:cellphone) }
+  it { should respond_to(:name) }
+  it { should have_many(:products) }
+  it { should accept_nested_attributes_for(:products) }
+  describe '.new' do
+    it { is_expected.to be_a_new(User) }
+  end
+
+  let(:user) { create(:user) }
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user) }
+  let(:arr) { [user1, user2] }
+
+  it 'has a valid factory' do
+    expect(user).to be_valid
+  end
+
+  describe '#name returns a string' do
+    it { expect(user.name).to eql('John Doe') }
+  end
+
+  describe '#expect_me' do
+    context 'No other Users exist' do
+      it { expect(User.except_me(user)).to match_array(Array.new(0)) }
+    end
+
+    context 'Users exist' do
+      it { expect(User.except_me(user)).to match_array(arr) }
+    end
+  end
+
 end
